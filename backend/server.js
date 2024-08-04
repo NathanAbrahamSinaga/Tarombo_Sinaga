b752c6abb1a68331be32c23c767a1e0ffa793478
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-05 01:39:25 +0700

    komit 9

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,4 @@ app.use('/api/family-members', familyMemberRoutes);
 app.use('/api/family-news', familyNewsRoutes);
 app.use('/api/family-diagram', familyDiagramRoutes);
 
-
-const PORT = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
-
 module.exports = app;
