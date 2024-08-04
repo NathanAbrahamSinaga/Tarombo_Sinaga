e1448e6415d3ccd0d5af7b9e40e1faabbedfbe4f
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-05 01:26:49 +0700

    komit 8

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(cors({
-    origin: '',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }
-));
+  origin: ['https://tarombo-sinaga.vercel.app', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json({ limit: '50mb' }));
@@ -38,4 +37,9 @@ app.use('/api/family-diagram', familyDiagramRoutes);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
