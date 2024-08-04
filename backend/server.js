d98ea645ea646985dead20f5438cfe89030c92e7
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-05 01:52:58 +0700

    komit 11

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,6 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(cors({
-  origin: ['https://tarombo-sinaga.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json({ limit: '50mb' }));
