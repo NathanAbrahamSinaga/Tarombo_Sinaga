66910d3950b43288b2b60d91e394fdf17eae3687
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-05 18:52:18 +0700

    kommit 35

--- frontend/src/pages/UserTaromboPage.js
+++ frontend/src/pages/UserTaromboPage.js
@@ -223,8 +223,8 @@ const UserTaromboPage = () => {
         </div>
       </header>
       <main className="container mx-auto mt-8">
-        <h2 className="text-xl sm:text-lg font-bold mb-4 text-[#8B0000] text-center">TAROMBO TOGA SINAGA - BONOR SUHUT NI HUTA</h2>
-        <h2 className="text-lg sm:text-md font-bold mb-4 text-[#8B0000] text-center">HUTA SOSOR LONTUNG, PARBABA DOLOK, SAMOSIR</h2>
+        <h2 className="text-xl sm:text-2xl font-bold mb-4 text-[#8B0000] text-center">TAROMBO TOGA SINAGA - BONOR SUHUT NI HUTA</h2>
+        <h2 className="text-lg sm:text-xl font-bold mb-4 text-[#8B0000] text-center">HUTA SOSOR LONTUNG, PARBABA DOLOK, SAMOSIR</h2>
         <div style={{ height: '600px', width: '100%' }} ref={reactFlowWrapper}>
           <ReactFlow
             nodes={memoizedNodes}
