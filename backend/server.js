8cef7ce32711d8f014d72a714b06ecae6bc3e824
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-06 08:13:27 +0700

    kommit 57

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.get('/', (req, res) => {
 });
 
 // Middleware
+app.use('/api/family-members/update-positions', cors());
+
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
