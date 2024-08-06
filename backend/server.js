31d9de8309d9bcabfd160baa6f722940033950b3
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-06 07:53:45 +0700

    kommit 55

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-// backend/server.js
-
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const authRoutes = require('./routes/auth');
 const familyMemberRoutes = require('./routes/familyMembers');
 const familyNewsRoutes = require('./routes/familyNews');
 const familyDiagramRoutes = require('./routes/familyDiagram');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 const app = express();
@@ -16,15 +16,8 @@ app.get('/', (req, res) => {
   res.send('Tarombo API is running');
 });
 
-// CORS Middleware
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://tarombo-sinaga.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json({ limit: '50mb' }));
@@ -40,6 +33,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/family-members', familyMemberRoutes);
 app.use('/api/family-news', familyNewsRoutes);
 app.use('/api/family-diagram', familyDiagramRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
