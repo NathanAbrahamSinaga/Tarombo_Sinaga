32dcf78dba85479834ea160fd7f0b887119d0459
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-08-08 16:27:37 +0700

    komit 88

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const authRoutes = require('./routes/auth');
@@ -19,12 +19,13 @@ app.get('/', (req, res) => {
 // Middleware
 app.use('/api/family-members/update-positions');
 
-// app.use(cors({
-//   origin: 'https://tarombo-sinaga.vercel.app',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: 'https://tarombo-sinaga.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json({ limit: '50mb' }));
